d15ed323296416fdd76c322f675e1b9d8c7f4782
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-20 13:38:42 -0300

    :test_tube: created a bunch of test cases for the function that scape sql characters

--- test.py
+++ test.py
@@ -1,14 +1,40 @@
 import unittest
 from csv_to_sql.csv_to_sql import format_sql_row
+from csv_to_sql.csv_to_sql import escape_sql_characters
 
 
 class TestFormatFunctions(unittest.TestCase):
+    def test_escape_sql_characters(self):
+        # Test case 1: Normal case
+        assert escape_sql_characters("Claire O'Connell") == "Claire O\\'Connell"
+        
+        # Test case 2: Special characters
+        assert escape_sql_characters("Claire O\"Connell") == "Claire O\\\"Connell"
+        assert escape_sql_characters("Claire O/Connell") == "Claire O\\/Connell"
+        assert escape_sql_characters("Claire O%Connell") == "Claire O\\%Connell"
+        assert escape_sql_characters("Claire O_Connell") == "Claire O\\_Connell"
+ 
+        # Test case 3: NULL, TRUE, FALSE values
+        assert escape_sql_characters("NULL") == "NULL"
+        assert escape_sql_characters("null") == "null"
+        assert escape_sql_characters("TRUE") == "TRUE"
+        assert escape_sql_characters("true") == "true"
+        assert escape_sql_characters("FALSE") == "FALSE"
+        assert escape_sql_characters("false") == "false"
+        
+        # Test case 4: Empty string
+        assert escape_sql_characters("") == ""
+        
+        # Test case 5: String with only spaces
+        assert escape_sql_characters("   ") == "   "
+
+    
     def test_format_sql_row(self):
         # Test case 1: Normal case
         assert format_sql_row(["52", "Rice", "20.7", "TRUE"]) == ["52", "E'Rice'", "20.7", "TRUE"]
         
         # Test case 2: Special characters
-        assert format_sql_row(["52", "Ri'ce", "20.7", "TRUE"]) == ["52", r"E'Ri'ce'", "20.7", "TRUE"]
+        assert format_sql_row(["52", "Ri'ce", "20.7", "TRUE"]) == ["52", r"E'Ri\'ce'", "20.7", "TRUE"]
         assert format_sql_row(["52", "Ri\"ce", "20.7", "TRUE"]) == ["52", "E'Ri\\\"ce'", "20.7", "TRUE"]
         assert format_sql_row(["52", "Ri/ce", "20.7", "TRUE"]) == ["52", r"E'Ri\/ce'", "20.7", "TRUE"]
         assert format_sql_row(["52", "Ri%ce", "20.7", "TRUE"]) == ["52", r"E'Ri\%ce'", "20.7", "TRUE"]
