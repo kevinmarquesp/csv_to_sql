b6612a14b2062af3d50ba15cff9cf8abc29a625f
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-20 10:16:45 -0300

    :art: created a function that shows colored text with a custom syntax str format

--- csv_to_sql/csv_to_sql.py
+++ csv_to_sql/csv_to_sql.py
@@ -4,7 +4,33 @@ from sys import argv
 from argparse import Namespace, ArgumentParser
 
 ## TODO: the arguments can not handle the db connection yet
-## TODO: fix the typos for the `parse_arguments/1` function
+## TODO: fix the typos for the `parse_arguments/1` function docstring
+## TODO: fix the typos for the `Cli.show/1` function docstring
+## TODO: fix the typos for the `Cli` class docstring
+
+
+class Cli:
+    """This `Cli` class means to act like an `namespace` in C, its only
+    propurse is to make this script more organized. This class holds all
+    methods that is related to the command line interface, such as showing
+    information on the screen or interacting with the user...
+    """
+    clr_codes = {
+        "[b]": "\033[30m", "[r]": "\033[31m", "[g]": "\033[32m",
+        "[y]": "\033[33m", "[b]": "\033[34m", "[m]": "\033[35m",
+        "[c]": "\033[36m", "[w]": "\033[37m", "[/]": "\033[m"
+    }
+
+    @staticmethod
+    def show(msg: str):
+        """Simple function that replaces some special characters to their
+        respective color codes, like replacing an `[g]`, which means "green",
+        to `\033[32m` of the string, then printing it on the screen.
+        """
+        for key, value in Cli.clr_codes.items():
+            msg = msg.replace(key, value)
+        print(msg)
+        
 
 
 def parse_arguments(args: list[str]) -> Namespace:
@@ -28,8 +54,11 @@ def parse_arguments(args: list[str]) -> Namespace:
 
 
 def main(args: list[str]) -> None:
-    parsed_args = parse_arguments(args)
-    print(parsed_args)
+    # parsed_args = parse_arguments(args)
+    # print(parsed_args)
+
+    Cli.show("[r]error:[/] something went wrong...")
+    Cli.show("[g]success:[/] good job!")
 
 
 if __name__ == "__main__":
